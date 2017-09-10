8aafa47ace9bcd16155c7fcd22c5904ad0a06c51
danidi <danieldial2010@hotmail.com>
2017-09-10 09:56:25 -0500

    cambios menu item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,42 @@ import{AddComponent as OrderAddComponet}from './orders/add/add.component';
 import{CustomersModule } from './customers/customers.module';
 import{OrdersModule}from './orders/orders.module';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule, 
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+  MdCoreModule,
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdPaginatorModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule, 
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+  
+  } from'@angular/material';
+
 import { AppComponent } from './app.component';
 import{MenuComponent} from './menu/menu.component';
 import {CurrentOptionComponent} from './current-option/current-option.component';
@@ -28,11 +64,43 @@ import {CurrentOptionComponent} from './current-option/current-option.component'
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CustomersModule,
     OrdersModule,
     RouterModule.forRoot(
-      appRoutes
-    ) 
+      appRoutes),
+      MdAutocompleteModule,
+      MdButtonModule,
+      MdButtonToggleModule, 
+      MdCardModule,
+      MdCheckboxModule,
+      MdChipsModule,
+      MdCoreModule,
+      MdDatepickerModule,
+      MdDialogModule,
+      MdExpansionModule,
+      MdGridListModule,
+      MdIconModule,
+      MdInputModule,
+      MdListModule,
+      MdMenuModule,
+      MdNativeDateModule,
+      MdPaginatorModule,
+      MdProgressBarModule,
+      MdProgressSpinnerModule, 
+      MdRadioModule,
+      MdRippleModule,
+      MdSelectModule,
+      MdSidenavModule,
+      MdSliderModule,
+      MdSlideToggleModule,
+      MdSnackBarModule,
+      MdSortModule,
+      MdTableModule,
+      MdTabsModule,
+      MdToolbarModule,
+      MdTooltipModule, 
+
     
 
   ],
