3a641cfa1b895154963e1d9bfe473bf02a8c7be8
danidi <danieldial2010@hotmail.com>
2017-09-10 08:10:48 -0500

    cambios de portada link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import{RouterModule,Routes} from '@angular/router';
+import{IndexComponent}from './customers/index/index.component';
+import{AddComponent as OrderAddComponet}from './orders/add/add.component';
+
+import{CustomersModule } from './customers/customers.module';
+import{OrdersModule}from './orders/orders.module';
+
 import { AppComponent } from './app.component';
+import{MenuComponent} from './menu/menu.component';
+import {CurrentOptionComponent} from './current-option/current-option.component';
+
+ const appRoutes: Routes = [
+   {path:'customers/index', component:IndexComponent},
+   {path:'orders/add', component: OrderAddComponet},
+
+
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CurrentOptionComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomersModule,
+    OrdersModule,
+    RouterModule.forRoot(
+      appRoutes
+    ) 
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
